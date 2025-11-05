9388a0eaf58efabc9f852b562cc752d041069f20
Arley G <arleyguerrero@blacksip.com>
2025-11-05 15:28:13 -0500

    Feat: Se agrega la funcionalidad touchable

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,23 @@
 import React from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  Alert,
+  StyleSheet,
+  Text,
+  View,
+  TouchableWithoutFeedback,
+} from "react-native";
 
 export default function HomeScreen() {
   return (
     <View style={styles.body}>
       <StatusBar style="auto" />
       <Text style={styles.title}>Hola BlackSip</Text>
+      <TouchableWithoutFeedback
+        onPress={() => Alert.alert("¿Por qué me tocaste?")}
+      >
+        <Text style={styles.alert}>No me vayas a tocar</Text>
+      </TouchableWithoutFeedback>
     </View>
   );
 }
@@ -17,9 +28,12 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     backgroundColor: "#000",
-    margin: "auto"
+    margin: "auto",
   },
   title: {
     color: "#FFF",
+  },
+  alert: {
+    color: "red",
   },
 });
